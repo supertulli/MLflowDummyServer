f7c8813705ee422db1d4d98685a4658639801a2a
Pedro Sousa <supertulli@gmail.com>
2020-01-24 17:10:07 +0000

    folder restructure

--- dummyMLproject/vectorizer.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from bert_serving.client import BertClient
-
-def vectorize(sentence_series, ip='localhost', port=5555):
-    bc = BertClient(show_server_config=True , ignore_all_checks=True, port=port, ip=ip)
-    return list(bc.encode(sentence_series.tolist()))
-    
